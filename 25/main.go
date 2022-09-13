95d10b8a6cee241035e765577a2b808ee13c8c32
北国风光 <zibin_guang@163.com>
2022-09-13 12:40:38 +0800

    feat-25-update

--- 25/main.go
+++ 25/main.go
@@ -1,7 +1,8 @@
-/* 
+/*
 	选取 receiver 参数的原则
 	1. 如果 go 方法要把 receiver 参数代表的类型实例的修改，反映到原类型实例上，那么就应该选择 *T 作为 receiver 参数的类型
 	2. 如果 receiver 参数类型的 size 较大，以值传递形式传入会有较大性能开销，这时我们也应该选择 *T 作为 receiver 参数的类型
+	3. T 类型是否需要实现某个接口。如果 T 类型需要实现某个接口，那我们就要使用 T 作为 receiver 参数的类型，来满足接口类型方法集合中的所有方法。
 */
 package main
 
@@ -19,8 +20,8 @@ func (t *T) M2() {
 	t.a = 11
 }
 
-func main1()  {
-	var t T = T	{a: 20}
+func main1() {
+	var t T = T{a: 20}
 	fmt.Println(t.a) // 20
 
 	// T 作为参数类型，传递的是 t 的副本，所以在 M1 中修改 t.a，原 t 的值并没有变化
@@ -33,13 +34,13 @@ func main1()  {
 	fmt.Println(t.a) // 10
 }
 
-func main2()  {
+func main2() {
 	/*
-	这是 go 的语法糖
-	编译器自动进行类型转换
-	无论是 T 类型实例，还是 *T 类型实例，
-	都既可以调用 receiver 为 T 类型的方法，
-	也可以调用 receiver 为 *T 类型的方法
+		这是 go 的语法糖
+		编译器自动进行类型转换
+		无论是 T 类型实例，还是 *T 类型实例，
+		都既可以调用 receiver 为 T 类型的方法，
+		也可以调用 receiver 为 *T 类型的方法
 	*/
 
 	var t1 T
@@ -57,7 +58,6 @@ func main2()  {
 	fmt.Println(t2.a)
 }
 
-
 type Interface interface {
 	M1()
 	M2()
@@ -68,10 +68,10 @@ type T2 struct{}
 func (t T2) M1() {
 }
 
-func (t *T2) M2()  {
+func (t *T2) M2() {
 }
 
-func main()  {
+func main() {
 	var t T
 	var pt *T
 	var i Interface
